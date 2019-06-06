278e8f5f28475643edb482090a3f25c61fcafef3
Noah Gary <noah.gary@healthcareassetnetwork.com>
2019-06-06 15:26:35 -0400

    adding some comments

--- webscraper.go
+++ webscraper.go
@@ -9,38 +9,34 @@ import (
 );
 
 func main () {
-	fmt.Printf("What website should I scrape? ");
+	//get site from user
+	fmt.Printf("Website to scrape: ");
 	var siteUrl string
 	fmt.Scan(&siteUrl)
 
+	//get jquery selector from user
 	fmt.Printf("\n");
-	fmt.Printf("What kind of element? ");
+	fmt.Printf("Jquery Selector:  ");
 	var element string
 	fmt.Scan(&element)
 
+	//get search term from user
 	fmt.Printf("\n");
-	fmt.Printf("Filter by? ");
+	fmt.Printf("Filter by: ");
 	var filter string
 	fmt.Scan(&filter)
 
-	fmt.Printf("\n");
-	fmt.Printf("Site: %v", siteUrl);
-	fmt.Printf("\n");
-	fmt.Printf("Element: %v", element);
-	fmt.Printf("\n");
-	fmt.Printf("Filter: %v", filter);
-	fmt.Printf("\n");
-
+	//add http:// if user doesn't provide it
 	var prepend string
 	if !strings.Contains(siteUrl, "http") {
 		prepend = "http://"
 	}
 
+	//make request
 	res, err := http.Get(prepend + siteUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
@@ -52,22 +48,16 @@ func main () {
 		log.Fatal(err)
 	}
 
-    // fmt.Printf("\n");
-
-	// fmt.Printf("Results:\n");
-	// doc.Find(element).Each(func(i int, s *goquery.Selection){
-	// 	fmt.Printf("%v\n", s.Text());
-	// });
-
+	//get filtered results
 	filteredResults := doc.Find(element).FilterFunction(func(i int, s *goquery.Selection) bool {
 		return strings.Contains(s.Text(), filter);
 	});
 
 	fmt.Printf("\n");
-	
-	fmt.Printf("Filtered:\n");
+	fmt.Printf("Filtered Results:");
 
 	filteredResults.Each(func(i int, s *goquery.Selection){
-		fmt.Printf("%v\n", s.Text());
+		fmt.Printf("\n");
+		fmt.Printf("%v. %v\n", i+1, s.Text());
 	});
 }
